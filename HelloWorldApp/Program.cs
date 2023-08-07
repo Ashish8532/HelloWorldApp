2d8fac2b1ac27fe843f97a3c0e1c35a2abfac9b3
Ashish8532 <ashishraj916263@outlook.com>
2023-08-07 19:14:11 +0530

    Added Comments

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -2,30 +2,37 @@
 
 class HelloWorld
 {
+    // Method to do Addition of two Numbers
     static void Sum(int a, int b)
     {
         int sum = a + b;
+        // Display Summation Result
         Console.WriteLine("Sum = " + sum);
     }
 
+    // Method to do Multiplication of two Numbers
     static void Multiply(int a, int b)
     {
         int result = a * b;
+        // Display Multiplication Result 
         Console.WriteLine("Multiplication = " + result);
     }
 
+    // Method to do Division of two Numbers
     static void Division(double x, double y)
     {
         double result = x / y;
+        // Display Division Result 
         Console.WriteLine("Division = " + result);
     }
 
+    // Main
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello World!\n");
-        Sum(1, 2);
-        Multiply(5, 4);
-        Division(10, 6);
+        Sum(1, 2); // Calling Sum Method
+        Multiply(5, 4); // Calling Multiply Method
+        Division(10, 6); // Calling Division Method
     }
     
 }
