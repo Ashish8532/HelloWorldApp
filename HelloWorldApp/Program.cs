ac750d0b376b1fd0157b556b84b5cd5641ba552f
Ashish8532 <ashishraj916263@outlook.com>
2023-08-07 19:16:29 +0530

    Added Comments

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -2,23 +2,27 @@
 
 class HelloWorld
 {
+    // Method to do Addition of two Numbers
     static void Sum(int a, int b)
     {
         int sum = a + b;
+        // Display Summation Result
         Console.WriteLine("Sum = " + sum);
     }
 
+    // Method to do Multiplication of two Numbers
     static void Multiply(int a, int b)
     {
         int result = a * b;
+        // Display Multiplication Result 
         Console.WriteLine("Multiplication = " + result);
     }
 
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello World!\n");
-        Sum(1, 2);
-        Multiply(5, 4);
+        Sum(1, 2); // Calling Sum Method
+        Multiply(5, 4); // Calling Multiply Method
     }
     
 }
