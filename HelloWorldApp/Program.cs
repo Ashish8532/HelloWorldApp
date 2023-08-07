4badcf083547569cfe52cac4b64d85176bc85f74
Ashish8532 <ashishraj916263@outlook.com>
2023-08-07 03:27:55 -0700

    Added Sum Method

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -2,8 +2,16 @@
 
 class HelloWorld
 {
+    static void Sum(int a, int b)
+    {
+        int sum = a + b;
+        Console.WriteLine("Sum = " + sum);
+    }
+
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!\n");
+        Sum(1, 2);
     }
+    
 }
